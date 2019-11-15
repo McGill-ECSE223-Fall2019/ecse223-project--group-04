b82715a8a2a02843da1d969fd9f66b8755c5ad20
Returrn-Null <47578902+Returrn-Null@users.noreply.github.com>
2019-11-15 15:46:31 -0500

    Revert "Dev"

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse223.quoridor;
 
+
 import ca.mcgill.ecse223.quoridor.model.Quoridor;
 import ca.mcgill.ecse223.quoridor.view.*;
 
@@ -8,14 +9,15 @@ public class QuoridorApplication {
 
 	private static Quoridor quoridor;
 
-	public static void main() {
-		try {
-			MainMenu menu = new MainMenu();
-			menu.frame.setVisible(true);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+	public static void main(String[] args) {
+		// start UI
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                MainMenu.main(args);
+            }
+        });
 	}
+	
 	public static Quoridor getQuoridor() {
 		if (quoridor == null) {
 			quoridor = new Quoridor();
